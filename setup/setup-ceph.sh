ef2a01599981778c9e82062d0cf3e05267d9abc3
Allen Porter <allen@thebends.org>
2021-02-27 14:41:19 -0800

    Update format for ceph monitors
    Fix the format for ceph monitors to include the hostname=ip:port format. While all hosts aren't necessarily, will include all of them anyway.

--- setup/setup-ceph.sh
+++ setup/setup-ceph.sh
@@ -16,6 +16,9 @@ NAMESPACE="rook-ceph"
 ROOK_EXTERNAL_FSID=$(ssh ${CEPH_HOST} sudo ceph fsid)
 ROOK_EXTERNAL_ADMIN_SECRET=$(ssh ${CEPH_HOST} sudo ceph auth get-key client.admin)
 # Pick the first monitor
-ROOK_EXTERNAL_CEPH_MON_DATA=$(ssh ${CEPH_HOST} sudo ceph mon dump | egrep "^[0-9]: " | sed 's/.*v1:\(.*:6789\).*/\1/' | head -1)
+ROOK_EXTERNAL_CEPH_MON_DATA=$(ssh ${CEPH_HOST} sudo ceph mon dump | egrep "^[0-9]: " | sed 's/.*v1:\(.*:6789\).*] mon.\(.*\)/\2=\1/' | tr '\n' ',' | sed 's/,$//')
+
+echo "ROOK_EXTERNAL_FSID=${ROOK_EXTERNAL_FSID}"
+echo "ROOK_EXTERNAL_CEPH_MON_DATA=${ROOK_EXTERNAL_CEPH_MON_DATA}"
 
 source <(curl -s https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/import-external-cluster.sh)
