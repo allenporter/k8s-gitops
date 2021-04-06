1fcdc364657551ef63d27e957e0b8b3e1759b084
Allen Porter <allen@thebends.org>
2021-04-06 00:31:49 -0700

    Add teslamate dashboards, copied into repo
    Add a shell script to copy dashboards to the repo.
    Issue #47

---
+++ scripts/download-dashboards.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Download latest versions of grafana dashboards
+
+set -e
+
+TESLAMATE_VERSION=v1.22.0
+TESLAMATE_BASE_URL=https://raw.githubusercontent.com/adriankumpf/teslamate/${TESLAMATE_VERSION}/grafana/dashboards/
+
+TESLAMATE_DASHBOARDS=(charge-level charges charging-stats drive-stats drives efficiency locations mileage overview projected-range states trip updates vampire-drain visited internal/charge-details internal/drive-details)
+TESLAMATE_DASHBOARD_DIR=home/base/teslamate/dashboards
+
+for DASHBOARD in ${TESLAMATE_DASHBOARDS[@]}; do
+  URL=${TESLAMATE_BASE_URL}${DASHBOARD}.json
+  echo "Fetching ${URL}"
+
+  DASHBOARD_NAME=${DASHBOARD//\//-}
+  OUTPUT_FILE="home/base/teslamate/dashboard-${DASHBOARD_NAME}.yaml"
+  JSON_DATA=$(curl -s $URL)
+
+  echo $OUTPUT_FILE
+
+  cat <<EOT >$OUTPUT_FILE
+# yamllint disable-file
+---
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: grafana-dashboard-${DASHBOARD_NAME}
+  namespace: teslamate
+  labels:
+    grafana_dashboard: 1
+data:
+  |-
+  ${JSON_DATA}
+EOT
+
+done
