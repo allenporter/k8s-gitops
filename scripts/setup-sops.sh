fb558f4adf6ed57e9354ebc6980abff8f8b6d433
Allen Porter <allen@thebends.org>
2021-08-23 22:22:11 -0700

    Create dev/prod sops public keys
    Following guide at https://fluxcd.io/docs/guides/mozilla-sops/
    Issue #45

---
+++ scripts/setup-sops.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+function get_secret() {
+  if [ "${CONTEXT}" == "template" ]; then
+    echo ""
+    return 0
+  fi
+  secret_name=$1
+  namespace=$2
+  key=$3
+  kubectl get secret ${secret_name} -n ${namespace} ${KUBECTL_FLAGS} -o jsonpath="{.data.${key}}" | base64 --decode
+  return 0
+}
+
+SECRET_NAME="sops-gpg"
+SECRET_NAMESPACE="flux-system"
+SECRET_KEY="sops.asc"
+
+SOPS_KEY_FP=$(get_secret "${SECRET_NAME}" "${SECRET_NAMESPACE}" "${SECRET_KEY}")
+if [ "${SOPS_KEY_FP}" != "" ]; then
+    echo Secret key already exists at "${SECRET_NAMESPACE}/${SECRET_NAME}"
+    exit 0
+fi
+
+KEY_NAME="${ENV}.mrv.thebends.org"
+KEY_COMMENT="flux secrets"
+
+echo "Checking if ${KEY_NAME} exists"
+ret=0
+gpg --list-secret-keys "${KEY_NAME}" || ret=$?
+if [ $ret -ne 0 ]; then
+    gpg --batch --full-generate-key <<EOF
+%no-protection
+Key-Type: 1
+Key-Length: 4096
+Subkey-Type: 1
+Subkey-Length: 4096
+Expire-Date: 0
+Name-Comment: ${KEY_COMMENT}
+Name-Real: ${KEY_NAME}
+EOF
+    gpg --list-secret-keys "${KEY_NAME}"
+fi
+
+
+KEY_ID=$(gpg --list-secret-keys --keyid-format LONG  "${KEY_NAME}" | awk '/sec/{if (length($2) > 0) print $2}')
+SOPS_KEY_FP=${KEY_ID##*/}
+
+gpg --export-secret-keys --armor "${SOPS_KEY_FP}" | kubectl create secret generic "${SECRET_NAME}" --namespace="${SECRET_NAMESPACE}" --from-file=${SECRET_KEY}=/dev/stdin
+
+echo Secret key created at "${SECRET_NAMESPACE}/${SECRET_NAME}"
