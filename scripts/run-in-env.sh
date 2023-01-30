ed0c6acf27f58569a207358d23e89cfd0b4b5317
Allen Porter <allen@thebends.org>
2023-01-30 07:56:28 +0000

    Generate a cluster manifest file

---
+++ scripts/run-in-env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+set -eu
+
+my_path=$(git rev-parse --show-toplevel)
+
+if [ -f "${my_path}/venv/bin/activate" ]; then
+  . "${my_path}/venv/bin/activate"
+  break
+fi
+
+exec "$@"
