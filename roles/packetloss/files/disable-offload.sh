53ac38ed9f3659c180f91d91a33c7ffea8c9471d
Allen Porter <allen@thebends.org>
2023-05-21 09:37:21 -0700

    Update packetloss related plays and alerts (#1120)
    * Update action name for correct repo
    
    * Update input variables for v0.1
    
    * Test v0.1
    
    * Update local test action
    
    * Increase duration of network packet loss rule to reduce spam
    
    * Streamline disable offoad to detect changes

--- roles/packetloss/files/disable-offload.sh
+++ roles/packetloss/files/disable-offload.sh
@@ -7,7 +7,11 @@ test -x $ETHTOOL || exit 1
 
 [ "$IFACE" != "lo" ] || exit 0
 
+# Exit if offload already disabled
+${ETHTOOL} -k ${IFACE} | grep "segmentation-offload: on" || exit 0
+
 echo "Disabling offload for ${IFACE}"
 ${ETHTOOL} -K ${IFACE} gso off
 ${ETHTOOL} -K ${IFACE} tso off
 ${ETHTOOL} --offload ${IFACE} tx off
+echo "UPDATED"
