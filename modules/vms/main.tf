2751047cffe957f2a1c01dfe6421d3729a89d3e8
Allen Porter <allen@thebends.org>
2023-12-02 16:37:54 -0800

    Revert terraform provider change (#1427)
    * Update terraform provider for vm hosts
    
    * Revert hostpci chnage
    
    * Move to new terraform provider
    
    * Fix terraform provider id
    
    * Revert terraform provider updates
    
    * Revert more proxmox provider changes

--- modules/vms/main.tf
+++ modules/vms/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "bpg/proxmox"
-      version = "0.39.0"
+      source  = "telmate/proxmox"
+      version = "2.9.14"
     }
   }
 }
