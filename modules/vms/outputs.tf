c0c35f879bf14b4d9991c1826459cfc537e12697
Allen Porter <allen.porter@gmail.com>
2023-12-09 23:48:21 +0000

    Updates for new proxmox api

--- modules/vms/outputs.tf
+++ modules/vms/outputs.tf
@@ -1,6 +1,6 @@
 output "node_id" {
   value = {
-    for instance in proxmox_vm_qemu.proxmox-vmm:
+    for instance in proxmox_virtual_environment_vm.proxmox-vmm:
       instance.name => element(split("/", instance.id), 2)
   }
 }
