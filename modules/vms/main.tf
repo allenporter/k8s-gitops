104eab60a74e4ac6b2bd6813ea75fafcf5d09342
Allen Porter <allen.porter@gmail.com>
2023-12-09 23:10:20 +0000

    Go back to newer terraform provider

--- modules/vms/main.tf
+++ modules/vms/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "TheGameProfi/proxmox"
-      version = "2.9.15"
+      source  = "bpg/proxmox"
+      version = "0.39.0"
     }
   }
 }
