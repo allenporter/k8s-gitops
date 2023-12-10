6bcf798960c670873b1216fa41f718e205683c7b
Allen Porter <allen.porter@gmail.com>
2023-12-10 04:17:50 +0000

    Update localize script

--- localize/localize.sh
+++ localize/localize.sh
@@ -5,20 +5,21 @@
 
 set -e
 
+
+rm -fr crds
+kustomize localize localize/crds crds
+
 rm -fr infrastructure/dev/calico
 kustomize localize localize/calico/dev infrastructure/dev/calico
 
 rm -fr infrastructure/prod/calico
 kustomize localize localize/calico/prod infrastructure/prod/calico
 
-rm -fr home/base/teslamate/dashboards
-kustomize localize localize/teslamate-monitoring home/base/teslamate/dashboards
-
 rm -fr monitoring/base/flux-monitoring/dashboards
 kustomize localize localize/flux-monitoring monitoring/base/flux-monitoring/dashboards
 
-rm -fr crds
-kustomize localize localize/crds crds
-
 rm -fr infrastructure/base/rook-ceph/snapshot-controller
 kustomize localize localize/snapshot-controller infrastructure/base/rook-ceph/snapshot-controller
+
+rm -fr home/base/teslamate/dashboards
+kustomize localize localize/teslamate-monitoring home/base/teslamate/dashboards
