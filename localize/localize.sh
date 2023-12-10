1cf43d01db353b495b1078114bc99571055d51e1
Allen Porter <allen@thebends.org>
2023-12-09 20:26:12 -0800

    Localize all remote files used in kustomizations (#1465)
    * Localize all remote files used in kustomizations
    
    * Exclude localized files from lint check
    
    * Update localize script
    
    * Add a localize readme
    
    * Update localize README

---
+++ localize/localize.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Script to run `kustomize localize` on all needed kustomization.yaml files in
+# the repo.
+
+set -e
+
+
+rm -fr crds
+kustomize localize localize/crds crds
+
+rm -fr infrastructure/dev/calico
+kustomize localize localize/calico/dev infrastructure/dev/calico
+
+rm -fr infrastructure/prod/calico
+kustomize localize localize/calico/prod infrastructure/prod/calico
+
+rm -fr monitoring/base/flux-monitoring/dashboards
+kustomize localize localize/flux-monitoring monitoring/base/flux-monitoring/dashboards
+
+rm -fr infrastructure/base/rook-ceph/snapshot-controller
+kustomize localize localize/snapshot-controller infrastructure/base/rook-ceph/snapshot-controller
+
+rm -fr home/base/teslamate/dashboards
+kustomize localize localize/teslamate-monitoring home/base/teslamate/dashboards
