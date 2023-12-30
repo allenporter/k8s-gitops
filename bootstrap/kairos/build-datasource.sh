986eb168dfc9f639a55eb2e7109d383c6cda2cfa
Allen Porter <allen.porter@gmail.com>
2023-12-30 19:23:33 +0000

    Update datasources for kairos demo

--- bootstrap/kairos/build-datasource.sh
+++ bootstrap/kairos/build-datasource.sh
@@ -5,7 +5,6 @@ set -e
 KAIROS_DIR="${PWD}/bootstrap/kairos"
 : ${BUILD_DIR:="${KAIROS_DIR}/build"}
 
-IMAGE="primary"
 for image in primary secondary; do
     if [ ! -f ${KAIROS_DIR}/cloud-config-${image}.yaml ]; then
         echo "Could not find image ${image} in ${KAIROS_DIR}"
@@ -13,11 +12,14 @@ for image in primary secondary; do
         IMAGE=${image}
         break
     fi
-    WORK_DIR="${BUILD_DIR}/${IMAGE}"
+    WORK_DIR="${BUILD_DIR}/${image}"
     mkdir -p ${WORK_DIR}
     touch ${WORK_DIR}/meta-data
-    cp -rfv ${KAIROS_DIR}/cloud-config-${IMAGE}.yaml ${WORK_DIR}/user-data
-    ISO="${BUILD_DIR}/${IMAGE}.iso"
+    cp -rfv ${KAIROS_DIR}/cloud-config-${image}.yaml ${WORK_DIR}/user-data
+    ISO="${BUILD_DIR}/${image}.iso"
+    echo
     echo "Building ${ISO}"
     mkisofs -output ${ISO} -volid cidata -joliet -rock ${WORK_DIR}/meta-data ${WORK_DIR}/user-data
+    echo "Finished building ${ISO}"
+
 done
