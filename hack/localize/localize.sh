a302c44dc43f164d020dda271cdf6973cb5104c9
Allen Porter <allen.porter@gmail.com>
2024-01-09 03:26:27 +0000

    Install system-upgrade-controller

--- hack/localize/localize.sh
+++ hack/localize/localize.sh
@@ -16,5 +16,8 @@ kustomize localize ${LOCALIZE_DIR}/calico/prod kubernetes/cluster_network/prod/c
 rm -fr kubernetes/monitoring/base/flux-monitoring/dashboards
 kustomize localize ${LOCALIZE_DIR}/flux-monitoring kubernetes/monitoring/base/flux-monitoring/dashboards
 
-rm -fr kubernetes/storage/prod/snapshot-controller
-kustomize localize ${LOCALIZE_DIR}/snapshot-controller kubernetes/storage/prod/snapshot-controller
+rm -fr kubernetes/cluster_storage/prod/snapshot-controller
+kustomize localize ${LOCALIZE_DIR}/snapshot-controller kubernetes/cluster_storage/prod/snapshot-controller
+
+rm -fr kubernetes/cluster_compute/prod/system-upgrade-controller
+kustomize localize ${LOCALIZE_DIR}/system-upgrade-controller kubernetes/cluster_compute/prod/system-upgrade-controller
