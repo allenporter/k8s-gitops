29d12d7de2ca419514550710587fbd54617f1f04
Allen Porter <allen.porter@gmail.com>
2024-01-09 11:51:32 -0800

    Verify cluster manifest in a github action (#1644)
    * Verify cluster manifest in a github action
    
    * Update manifest name
    
    * Touch diff
    
    * Remove unnecessary dependencies
    
    * Fail when manifest changes
    
    * Update taskfile and manifest script to fail on diff
    
    * Run validate command in CI
    
    * Remove diffs from manifest

--- hack/update-manifest.sh
+++ hack/update-manifest.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env sh
-set -eu
+#
+# The script will either update or verify the manifest file. Pass the
+# argument "generate" to update the manifest file. Pass the argument "validate"
+# to verify the manifest file. The default with no argument is to generate.
 
-flux-local get cluster -o yaml --path kubernetes/clusters/prod > kubernetes/clusters/manifest.yaml
+COMMAND=${1:-generate}
+if [ "$COMMAND" != "generate" ] && [ "$COMMAND" != "validate" ]; then
+  echo "Invalid command. Please pass either 'generate' or 'validate'."
+  exit 1
+fi
+
+MANIFEST_FILE=kubernetes/clusters/manifest.yaml
+if [ ! -f "$MANIFEST_FILE" ]; then
+  echo "Manifest file not found. Please run this script from the root of the repository."
+  exit 1
+fi
+
+# Validate the existing manifest and fail if there are diffs
+if [ "$COMMAND" = "validate" ]; then
+  flux-local get cluster -o yaml --path kubernetes/clusters/prod | diff - ${MANIFEST_FILE}
+  code=$?
+  if [ ${code} -ne 0 ]; then
+    echo "Manifest file is not up to date. Please run 'task update-manifest' to update it."
+  fi
+  exit $code
+fi
+
+# Update manifest
+flux-local get cluster -o yaml --path kubernetes/clusters/prod > ${MANIFEST_FILE}
