7517b11ccbb9f6e5c051efaba58252d2215b2e73
Allen Porter <allen.porter@gmail.com>
2024-01-09 18:00:34 +0000

    Fail when manifest changes

--- hack/update-manifest.sh
+++ hack/update-manifest.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env sh
 set -eu
 
-flux-local get cluster -o yaml --path kubernetes/clusters/prod > kubernetes/clusters/manifest.yaml
+MANIFEST_FILE=kubernetes/clusters/manifest.yaml
+
+md5sum_before=$(md5sum ${MANIFEST_FILE}| awk '{print $1}')
+
+# Update manifest
+flux-local get cluster -o yaml --path kubernetes/clusters/prod > ${MANIFEST_FILE}
+
+md5sum_after=$(md5sum ${MANIFEST_FILE} | awk '{print $1}')
+
+# Fail if the manifest was updated
+if [ "$md5sum_before" != "$md5sum_after" ]; then
+  echo "Manifest was updated. Please commit the changes."
+  exit 1
+fi
