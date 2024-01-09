fe151a8f4f81d8feed84349a462e750ea6f9252e
Allen Porter <allen.porter@gmail.com>
2024-01-09 16:43:21 +0000

    Rename compute, storage, network to remove cluster prefix

--- hack/localize/localize.sh
+++ hack/localize/localize.sh
@@ -10,14 +10,14 @@ LOCALIZE_DIR="hack/localize"
 rm -fr kubernetes/crds
 kustomize localize ${LOCALIZE_DIR}/crds kubernetes/crds
 
-rm -fr kubernetes/cluster_network/prod/calico
-kustomize localize ${LOCALIZE_DIR}/calico/prod kubernetes/cluster_network/prod/calico
+rm -fr kubernetes/network/prod/calico
+kustomize localize ${LOCALIZE_DIR}/calico/prod kubernetes/network/prod/calico
 
-rm -fr kubernetes/monitoring/base/flux-monitoring/dashboards
-kustomize localize ${LOCALIZE_DIR}/flux-monitoring kubernetes/monitoring/base/flux-monitoring/dashboards
+rm -fr kubernetes/storage/prod/snapshot-controller
+kustomize localize ${LOCALIZE_DIR}/snapshot-controller kubernetes/storage/prod/snapshot-controller
 
-rm -fr kubernetes/cluster_storage/prod/snapshot-controller
-kustomize localize ${LOCALIZE_DIR}/snapshot-controller kubernetes/cluster_storage/prod/snapshot-controller
+rm -fr kubernetes/compute/prod/system-upgrade-controller
+kustomize localize ${LOCALIZE_DIR}/system-upgrade-controller kubernetes/compute/prod/system-upgrade-controller
 
-rm -fr kubernetes/cluster_compute/prod/system-upgrade-controller
-kustomize localize ${LOCALIZE_DIR}/system-upgrade-controller kubernetes/cluster_compute/prod/system-upgrade-controller
+# rm -fr kubernetes/monitoring/base/flux-monitoring/dashboards
+# kustomize localize ${LOCALIZE_DIR}/flux-monitoring kubernetes/monitoring/base/flux-monitoring/dashboards
