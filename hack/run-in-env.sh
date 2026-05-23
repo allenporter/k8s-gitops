283a2fb37d132fee2c75adaf563ee9b994292c5a
Allen Porter <allen.porter@gmail.com>
2026-05-23 14:42:40 -0700

    feat: migrate local development environment to homebrew and uv

--- hack/run-in-env.sh
+++ hack/run-in-env.sh
@@ -3,9 +3,12 @@ set -eu
 
 my_path=$(git rev-parse --show-toplevel)
 
-if [ -f "${my_path}/venv/bin/activate" ]; then
+if [ -f "${my_path}/.venv/bin/activate" ]; then
+  . "${my_path}/.venv/bin/activate"
+  exec "$@"
+elif [ -f "${my_path}/venv/bin/activate" ]; then
   . "${my_path}/venv/bin/activate"
-  break
+  exec "$@"
+else
+  exec uv run "$@"
 fi
-
-uv run "$@"
